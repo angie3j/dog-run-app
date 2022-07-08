a1398a13e6404aa006cd57ea3ab656f7cb15f547
Angie Guerrero <angieg@Angies-MBP.fios-router.home>
2022-07-07 22:05:26 -0400

    Dog Owners JSONData File

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 // Bring in app
-const app = require('./app');
+const app = require("./app");
 
 require("dotenv").config()
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 // Start our server
 // This function 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
